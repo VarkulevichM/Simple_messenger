d43a76adf840e9dcd1ceefb3cfe7bf2f10f714ee
Михаил Варкулевич <workmailkra@gmail.com>
2025-02-19 15:59:09 +0700

    Доработка скрипта создания бд

--- app/db/session.py
+++ app/db/session.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import setting
 from app.core.log_file import logger
+from app.db.models.message import Base
 
 DATABASE_URL = f"postgresql+asyncpg://{setting.POSTGRES_USER}:{setting.POSTGRES_PASSWORD}@{setting.POSTGRES_HOST}:{setting.POSTGRES_PORT}/{setting.POSTGRES_DB}"
 
@@ -15,6 +16,18 @@ async_session = sessionmaker(
     class_=AsyncSession,
     expire_on_commit=False,
 )
+
+
+async def init_db():
+    """Создание всех таблиц в базе данных."""
+    try:
+        # создаем все таблицы, если их нет
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+        logger.info("База данных и таблицы успешно созданы.")
+    except Exception as e:
+        logger.error(f"Ошибка при создании базы данных: {str(e)}")
+        raise Exception("Ошибка при создании базы данных") from e
 
 
 async def get_db():
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.api.v1.endpoints.messages import router
 from app.core.log_file import log_request
 from app.core.metrics import metrics_app
+from app.db.session import init_db
 
 app = FastAPI(title="Simple messenger")
 
@@ -11,3 +12,9 @@ app.middleware("http")(log_request)
 app.include_router(router, prefix="/api/v1")
 
 app.mount("/metrics", metrics_app)
+
+
+@app.on_event("startup")
+async def startup():
+    """Инициализация базы данных при старте приложения"""
+    await init_db()
