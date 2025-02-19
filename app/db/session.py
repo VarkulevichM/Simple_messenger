ef38399b324a1306e1dab4a978e4fb64266ab7b8
Михаил Варкулевич <workmailkra@gmail.com>
2025-02-19 16:15:38 +0700

    Доработка скрипта создания бд

--- app/db/session.py
+++ app/db/session.py
@@ -1,3 +1,4 @@
+from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
@@ -19,15 +20,28 @@ async_session = sessionmaker(
 
 
 async def init_db():
-    """Создание всех таблиц в базе данных."""
+    """Создание базы данных и таблиц в базе данных."""
     try:
-        # создаем все таблицы, если их нет
+        # Шаг 1: Создание базы данных, если она не существует
+        async with engine.connect() as conn:
+            result = await conn.execute(text(
+                f"SELECT 1 FROM pg_database WHERE datname = '{setting.POSTGRES_DB}'"))
+            if not result.fetchone():
+                # База данных не существует, создаем её
+                await conn.execute(text(f"CREATE DATABASE {setting.POSTGRES_DB}"))
+                logger.info(
+                    f"База данных {setting.POSTGRES_DB} успешно создана.")
+            else:
+                logger.info(
+                    f"База данных {setting.POSTGRES_DB} уже существует.")
+
+        # Шаг 2: Создание таблиц в базе данных
         async with engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
-        logger.info("База данных и таблицы успешно созданы.")
+        logger.info("Таблицы успешно созданы.")
     except Exception as e:
-        logger.error(f"Ошибка при создании базы данных: {str(e)}")
-        raise Exception("Ошибка при создании базы данных") from e
+        logger.error(f"Ошибка при создании базы данных или таблиц: {str(e)}")
+        raise Exception("Ошибка при создании базы данных или таблиц") from e
 
 
 async def get_db():
